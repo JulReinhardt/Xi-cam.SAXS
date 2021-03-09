3cddd998e9f02f6d94dec4473b7b860773d9f291
Juliane Reinhardt <jreinhardt@lbl.gov>
2021-03-09 07:19:56 -1000

    testing overlay

--- xicam/SAXS/masking/threshold.py
+++ xicam/SAXS/masking/threshold.py
@@ -12,7 +12,11 @@ from xicam.plugins.operationplugin import operation, output_names, display_name,
 @describe_input('maximum', 'Threshold ceiling')
 @describe_input('neighborhood', 'Neighborhood size in pixels for morphological opening. Only clusters of this size \
                             that fail the threshold are masked')
-@intent(OverlayIntent, name='threshold', output_map={'image': 'threshold_mask'})
+@describe_output('mask', 'mask array for overlay')
+@describe_output('image', 'original image')
+@output_names('threshold_mask', 'images')
+#FIXME: What values in output_map?
+@intent(OverlayIntent, name='threshold', output_map={'image': 'images', 'mask': 'threshold_mask'})
 @visible('images', False)
 @categories('Masking')
 #TODO add the other decorators
@@ -25,14 +29,14 @@ def threshold_mask(images: np.ndarray,
     if maximum is None:
         maximum = np.max(images)
 
-    mask = np.logical_or(images < minimum, images > maximum)
+    threshold_mask = np.logical_or(images < minimum, images > maximum)
 
     y, x = np.ogrid[-neighborhood:neighborhood + 1,
                     -neighborhood:neighborhood + 1]
     kernel = x ** 2 + y ** 2 <= neighborhood ** 2
 
-    morphology.binary_opening(mask, kernel, output=mask)  # write-back to mask
-    if mask is not None:
-        mask = np.logical_or(mask, mask)  # .astype(np.int, copy=False)
-    return mask
+    morphology.binary_opening(threshold_mask, kernel, output=threshold_mask)  # write-back to mask
+    if threshold_mask is not None:
+        threshold_mask = np.logical_or(threshold_mask, threshold_mask)  # .astype(np.int, copy=False)
+    return threshold_mask, images
 
